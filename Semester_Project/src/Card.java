f0726e6ab4745cede5d6d43b277fd8eee5b2bf47
Amanda Williams <willas@farmingdale.edu>
2019-04-06 09:35:22 -0400

    Card and Suit classes, establishing inheritance

--- Semester_Project/src/Card.java
+++ Semester_Project/src/Card.java
@@ -1,4 +1,12 @@
 
-public class Card {
-
+public abstract class Card {
+	private int number;
+	private String pic;
+	
+	 public Card(String pic, int number) {
+	    this.pic = pic;
+		this.number = number;
+	}
+	 
+	 public void setSuit() {};
 }
--- Semester_Project/src/Spade.java
+++ Semester_Project/src/Spade.java
@@ -1,4 +1,18 @@
 
-public class Spade {
-
+public class Spade extends Card{
+	
+	public String suit;
+	
+	public Spade(String pic, int number, String suit) {
+		super(pic, number);
+		this.suit = suit;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+	
+	
+	
 }
